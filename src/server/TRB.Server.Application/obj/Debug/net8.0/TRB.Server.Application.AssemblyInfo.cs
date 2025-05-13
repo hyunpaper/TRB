056b92d71a19eeb2b0620755bf3c91bdf685c2c0
hyunpaper <mw2815@naver.com>
2025-05-14 02:04:40 +0900

    Branch 정리

--- src/server/TRB.Server.Application/obj/Debug/net8.0/TRB.Server.Application.AssemblyInfo.cs
+++ src/server/TRB.Server.Application/obj/Debug/net8.0/TRB.Server.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TRB.Server.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dabb7046fdbfbbaee3141057791fee980301f702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae7c6404faf8d409bbcbb2a965db9095d3b6ed2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TRB.Server.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TRB.Server.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
