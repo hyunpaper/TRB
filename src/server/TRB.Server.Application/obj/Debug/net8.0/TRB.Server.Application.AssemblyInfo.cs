f55ca297b7dd5b6375f772cbba1842ba25368d26
hyunpaper <mw2815@naver.com>
2025-05-18 02:32:21 +0900

    no message

--- src/server/TRB.Server.Application/obj/Debug/net8.0/TRB.Server.Application.AssemblyInfo.cs
+++ src/server/TRB.Server.Application/obj/Debug/net8.0/TRB.Server.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TRB.Server.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e924a4d5b197e890ae15c7cdd59327c49d56c275")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27b92ae05ca1b24e607661c75bd5949551c68f1c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TRB.Server.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TRB.Server.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
